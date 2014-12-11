004de06b4b1ca2f2d437935614403087771f9446
Khuong Dang <kuddox.dang@gmail.com>
2014-12-10 17:10:33 -0700

    Chapter 7 finished

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,4 +1,16 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
+	test "should get new" do
+		get :new
+		assert_response :success
+	end
+
+	#test "should display errors" do
+		#user_data = {name: 'Some Name', email: 'email@example.com', password: 'foobar', password_confirmation: 'foobar'}
+		#post :create, user: user_data
+		#assert_select 'div#error_explanation' do assert_select 'li', { count: 1 }
+		#end
+	#end
+
 end
